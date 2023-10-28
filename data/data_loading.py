e45651344c433a419f6fa0f94eef528dc8652998
SpectreSpect <spectrefromthefuture@gmail.com>
2023-10-29 02:47:19 +0400

    now it's possible to download reviews from different apps at the same time

--- data/data_loading.py
+++ data/data_loading.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import keyboard
 import random
+import numpy as np
 from timeit import default_timer as timer
 
 def to_percent_encoding(input_str: str):
@@ -44,90 +45,108 @@ class SteamWorks:
     #            f"cursor={cursor}"
 
     @staticmethod
-    def get(app_id, num_per_page=100, language='english', filter_='updated', cursor='*', **kwargs):
-        url = f"https://store.steampowered.com/appreviews/{app_id}?" \
+    def get(app_id, command='appreviews', num_per_page=100, language='english', filter_='updated', cursor='*', **kwargs):
+        url = f"https://store.steampowered.com/{command}/{app_id}?" \
                f"json=1&" \
                f"filter={filter_}&" \
                f"num_per_page={num_per_page}&" \
                f"language={language}&" \
                f"cursor={cursor}"
-        # print(url)
         return requests.get(url)
 
     def stop_loading_reviews(self, e):
         self.reviews_loading_force_stop = True
-        print('')
 
     def stop_downloading_reviews_to_csv(self, e):
         self.reviews_downloading_to_csv_force_stop = True
-        print('')
 
-    def get_reviews_and_voted_ups(self, app_id, cursor='*',
-                                  max_reviews_in_memory=10000,
-                                  downloaded_reviews_count=0, **kwargs):
-        cursors = []
+    def download_reviews(self, app_id,
+                         columns=['recommendationid', 'review', 'voted_up'], 
+                         cursor='*',
+                         max_reviews_in_memory=10000,
+                         downloaded_reviews_count=0,
+                         verbose=1,
+                         **kwargs):
+        cursors = [cursor]
         response = SteamWorks.get(app_id)
-        total_reviews = response.json()['query_summary']['total_reviews']
+        total_reviews = -1
+        if 'total_reviews' in response.json()['query_summary']:
+            total_reviews = response.json()['query_summary']['total_reviews']
         current_total_reviews = 0
         rows = []
+        num_per_page = 100
 
         keyboard.on_press_key("c", self.stop_loading_reviews)
-
+        found_duplicate_cursor = False
         while True:
             if self.reviews_loading_force_stop or self.reviews_downloading_to_csv_force_stop:
-                print('Loading was forcefully stopped')
+                if verbose != 0:
+                    print('Loading was forcefully stopped')
                 self.reviews_loading_force_stop = False
                 break
 
             if current_total_reviews >= max_reviews_in_memory:
-                print(f'Maximum number of reviews ({max_reviews_in_memory}) in memory has been exceeded.')
+                if verbose != 0:
+                    print(f'Maximum number of reviews ({max_reviews_in_memory}) in memory has been exceeded.')
                 break
-
-            response = SteamWorks.get(app_id, cursor=to_percent_encoding(cursor))
+            
+            if current_total_reviews + num_per_page > max_reviews_in_memory:
+                break
+            
+            response = SteamWorks.get(app_id, num_per_page=num_per_page, cursor=to_percent_encoding(cursor))
             response_json = response.json()
             success = response_json['success']
             if success == 1:
                 cursor = response_json['cursor']
 
                 if cursor in cursors:
-                    print(f'Found a duplicate cursor: {cursor}')
+                    print(f'(app {app_id}) Found a duplicate cursor: {cursor}')
+                    found_duplicate_cursor = True
                     break
                 cursors.append(cursor)
 
                 num_reviews = response_json['query_summary']['num_reviews']
                 current_total_reviews += num_reviews
                 downloaded_reviews_count += num_reviews
-                print(f'    downloaded: {downloaded_reviews_count}/{total_reviews}, '
-                      f'reviews_in_memory: {current_total_reviews}, '
-                      f'cursor: {cursor}')
+                if verbose != 0:
+                    print(f'    downloaded: {downloaded_reviews_count}/{total_reviews}, '
+                        f'reviews_in_memory: {current_total_reviews}, '
+                        f'cursor: {cursor}')
                 for j in range(num_reviews):
-                    recommendation_id = response_json['reviews'][j]['recommendationid']
-                    review = response_json['reviews'][j]['review']
-                    voted_up = response_json['reviews'][j]['voted_up']
-                    new_row = {'app_id': app_id, 'recommendationid': recommendation_id,
-                               'review': review, 'voted_up': voted_up}
+                    
+                    new_row = {}
+                    for k in range(len(columns)):
+                        new_row[columns[k]] = response_json['reviews'][j][columns[k]]
                     rows.append(new_row)
 
                 if current_total_reviews >= total_reviews:
+                    
                     break
             else:
-                print('Request failed')
-                print(f"error: {response_json['error']}")
+                error = response_json["error"]
+                print(f'Request failed with the following error: "{error}"')
                 break
 
         data_frame = pd.DataFrame(rows, columns=['app_id', 'recommendationid', 'review', 'voted_up'])
         return {'data_frame': data_frame,
                 'last_cursor': cursors[-1],
                 'downloaded_reviews_count': current_total_reviews,
-                'total_reviews': total_reviews}
+                'found_duplicate_cursor': found_duplicate_cursor
+                }
 
-    def download_reviews_to_csv(self, path, app_id, cursor="*", max_reviews_in_memory=5000, **kwargs):
+
+    def download_reviews_to_csv(self, path, 
+                                app_id,
+                                columns=['recommendationid', 'review', 'voted_up'], 
+                                cursor="*", 
+                                max_reviews_in_memory=10000, 
+                                **kwargs):
         file_id = random.randint(0, 10 ** 10)
 
         keyboard.on_press_key("q", self.stop_downloading_reviews_to_csv)
         last_cursor = cursor
         total_downloaded_reviews_count = 0
-        total_reviews = -1
+        total_reviews = self.get_total_reviews_count(app_id)
         total_elapsed_time = 0
         i = 0
         while True:
@@ -136,41 +155,139 @@ class SteamWorks:
                 print('Downloading reviews to csv was forcefully stopped.')
                 break
 
-
-
             start = timer()
-            reviews_data = self.get_reviews_and_voted_ups(app_id,
-                                                          cursor=last_cursor,
-                                                          max_reviews_in_memory=max_reviews_in_memory,
-                                                          downloaded_reviews_count=total_downloaded_reviews_count)
+            reviews_data = self.download_reviews(app_id,
+                                                 columns=columns,
+                                                 cursor=last_cursor,
+                                                 max_reviews_in_memory=max_reviews_in_memory,
+                                                 downloaded_reviews_count=total_downloaded_reviews_count)
             end = timer()
             elapsed_time = end - start
+            
             total_elapsed_time += elapsed_time
             data_frame = reviews_data['data_frame']
             total_downloaded_reviews_count += reviews_data['downloaded_reviews_count']
-            total_reviews = reviews_data['total_reviews']
             last_cursor = reviews_data['last_cursor']
+            
             print(f'Saving data... ')
             print(f"Reviews downloaded: {total_downloaded_reviews_count}/{total_reviews}, "
                   f"last_cursor: {last_cursor}, "
                   f"elapsed_time: {elapsed_time}, "
                   f"total_elapsed_time: {total_elapsed_time}")
-            if i <= 0:
-                data_frame.to_csv(f'{path}/app_id{app_id}file_id{file_id}.csv', index=False)
-            else:
-                data_frame.to_csv(f'{path}/app_id{app_id}file_id{file_id}.csv', index=False, mode='a', header=False)
+            
+            data_frame.to_csv(f'{path}/app_id{app_id}file_id{file_id}.csv', 
+                                index=False, 
+                                mode=('w' if not i else 'a'), 
+                                header=(not i))
             del data_frame
 
             if total_downloaded_reviews_count >= total_reviews:
                 print(f"Downloading has been completed ({total_downloaded_reviews_count}/{total_reviews}). Yay!! ^-^")
                 break
-
             i += 1
         print(f"Reviews downloaded: {total_downloaded_reviews_count}/{total_reviews}, "
               f"Last cursor: {last_cursor}")
+    
+    
+    def get_total_reviews_count(self, app_id):
+        return self.get(app_id).json()['query_summary']['total_reviews']
+    
+    
+    def download_reviews_to_csv_several_ids(self, path, 
+                                app_id: list,
+                                columns=['recommendationid', 'review', 'voted_up'], 
+                                cursor="*", 
+                                max_reviews_in_memory=3000, 
+                                **kwargs):
+        apps_count = len(app_id)
+        print("Downloading reviews from ", end="")
+        for i in range(apps_count):
+            if i < apps_count - 1:
+                print(f"{app_id[i]}, ", end="")
+            else:
+                print(f"{app_id[i]}.")
+        
+        file_id = random.randint(0, 10 ** 10)
+
+        keyboard.on_press_key("q", self.stop_downloading_reviews_to_csv)
+        last_cursors = np.full(shape=len(app_id), fill_value="*").tolist()
+        downloaded_reviews_count = 0
+        downloaded_reviews_count_per_app = np.full(shape=len(app_id), fill_value=0)
+        downloading_completed = np.full(shape=len(app_id), fill_value=False).tolist()
+        
+        total_reviews_count = 0
+        total_reviews_count_per_app = []
+        for id in app_id:
+            total_reviews_count_per_app.append(self.get_total_reviews_count(id))
+            total_reviews_count += total_reviews_count_per_app[-1]
+        
+        max_reviews_in_memory_per_app = max_reviews_in_memory / len(app_id)
+        
+        elapsed_time = 0
+        reviews_data = np.full(shape=len(app_id), fill_value=0).tolist()
+        i = 0
+        while True:
+            if self.reviews_downloading_to_csv_force_stop:
+                self.reviews_downloading_to_csv_force_stop = False
+                print('Downloading reviews to csv was forcefully stopped.')
+                break
+
+            
+            
+            for x in range(len(app_id)):
+                if not self.reviews_downloading_to_csv_force_stop:
+                    if not downloading_completed[x]:
+                        start = timer()
+                        rd = self.download_reviews(app_id[x],
+                                                columns=columns,
+                                                cursor=last_cursors[x],
+                                                verbose=0,
+                                                max_reviews_in_memory=max_reviews_in_memory_per_app,
+                                                downloaded_reviews_count=downloaded_reviews_count_per_app[x])
+                        reviews_data[x] = rd
+                        
+                        downloaded_reviews_count_per_app[x] += reviews_data[x]['downloaded_reviews_count']
+                        downloaded_reviews_count += reviews_data[x]['downloaded_reviews_count']
+                        last_cursors[x] = reviews_data[x]['last_cursor']
+                        
+                        reviews_data[x]['data_frame'].to_csv(f'{path}/app_id{app_id}file_id{file_id}.csv', 
+                                        index=False, 
+                                        mode=('w' if not i else 'a'), 
+                                        header=(not i))
+                        del reviews_data[x]['data_frame']
+                        
+                        end = timer()
+                        elapsed_time += (end - start) / 60.0
+                        
+                        time_left = (elapsed_time / downloaded_reviews_count) * total_reviews_count - elapsed_time
+                        
+                        print(f"{downloaded_reviews_count}/{total_reviews_count}    "
+                            f"app reviews downloaded: {downloaded_reviews_count_per_app[x]}/{total_reviews_count_per_app[x]}   " 
+                            f"last cursor: {last_cursors[x]}    app: {app_id[x]}    "
+                            f"elapsed time: {round(elapsed_time, 2)} min    "
+                            f"~{round(time_left, 2)} minutes left")
+                        
+                        
+                        if (downloaded_reviews_count_per_app[x] >= total_reviews_count_per_app[x] or 
+                            reviews_data[x]['found_duplicate_cursor'] or
+                            not (False in downloading_completed)):
+                            
+                            downloading_completed[x] = True
+                            print("\n" + "-" * 30)
+                            print(f"Downloading reviews from {app_id[x]} has been completed:"
+                                f"{downloaded_reviews_count_per_app[x]}/{total_reviews_count_per_app[x]}.")
+                            print("-" * 30 + "\n")
+
+            if downloaded_reviews_count >= total_reviews_count or not (False in downloading_completed):
+                break
+            i += 1
+        print(f"Downloading has been completed ({downloaded_reviews_count}/{total_reviews_count}). Yay!! ^-^")
 
 
 if __name__ == '__main__':
-    app_id = 1245620    # Elden Ring
+    # app_id = 1245620    # Elden Ring
+    
+    app_ids = [1245620, 570, 292030, 570940, 236430, 374320, 367520, 435150, 1888160, 814380, 730, 588650]
+    
     steam_works = SteamWorks()
-    steam_works.download_reviews_to_csv('reviews', app_id)
+    steam_works.download_reviews_to_csv_several_ids('reviews', app_ids)
